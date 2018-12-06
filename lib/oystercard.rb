a0e4dd5106d8109e1396c89963d39070e5d6b6b1
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2018-12-06 17:30:54 +0000

    Step 14 - New files created for station but tests not passing

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,17 +1,14 @@
+require_relative "./journey"
+
 class Oystercard
 
-  attr_reader :balance, :entry_station, :exit_station, :journeys
+  attr_reader :balance
 
   MAX_LIMIT = 90
-  MIN_LIMIT = 1
-
 
   def initialize
     @balance = 0
-    @status = false
-    @entry_station = nil
-    @exit_station = nil
-    @journeys = []
+
   end
 
   def topup(money)
@@ -19,33 +16,6 @@ class Oystercard
     @balance += money
   end
 
-  def touch_in(entry_station)
-    raise "YOU SHALL NOT PASSSSSSS" if @balance < MIN_LIMIT
 
-    @status = true
-    @exit_station = nil
-    @entry_station = entry_station
-  end
-
-  def touch_out(exit_station)
-    deduct
-    @status = false
-    @journeys << {start: entry_station, end: exit_station}
-    @entry_station = nil
-    @exit_station = exit_station
-  end
-
-  def in_journey?
-    if @entry_station != nil
-      @status == true
-    else
-      @exit_station == nil
-    end
-  end
-
-private
-    def deduct
-      @balance -= MIN_LIMIT
-    end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,18 +1,13 @@
 require "oystercard"
+require "journey"
 
 describe Oystercard do
 
-  let (:entry_station) {double :entry_station}
-  let (:exit_station) {double :exit_station}
 
   it { is_expected.to respond_to(:balance) }
 
   it "checks that initialized balance is 0" do
     expect(subject.balance).to eq(0)
-  end
-
-  it "checks that we have an empty list of journeys as default" do
-    expect(subject.journeys).to be_empty
   end
 
 
@@ -28,41 +23,9 @@ describe Oystercard do
       expect{ subject.topup(10) }.to raise_error "Sorry, limit exceeded!"
     end
 
-   context "#touch_in" do
-
-     it "should raise an error if balance is less than £1 on touch in" do
-       expect{ subject.touch_in(:entry_station) }.to raise_error "YOU SHALL NOT PASSSSSSS"
-     end
-
-     it "should set the entry station name on touch in" do
-       subject.topup(2)
-       expect(subject.touch_in(:entry_station)).to eq (:entry_station)
-     end
-   end
-
-   context "#touch_out" do
-
-     it "should deduct the correct fare on touch out" do
-       expect {subject.touch_out(:exit_station)}.to change{subject.balance}.by -Oystercard::MIN_LIMIT
-     end
-
-     it "should remember the exit station name on touch out" do
-       expect(subject.touch_out(:exit_station)).to eq (:exit_station)
-     end
-   end
 
 
-   it "should return true if we are in journey" do
-     subject.topup(2)
-     subject.touch_in(:entry_station)
-     expect(subject.in_journey?).to eq true
-   end
 
-   it "should add a completed journey to the journey hash" do
-     subject.topup(2)
-     subject.touch_in(:entry_station)
-     subject.touch_out(:exit_station)
-     expect(subject.journeys).to include(start: :entry_station, end: :exit_station)
-   end
+
 
 end
