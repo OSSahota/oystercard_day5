f45bf0a32ab6a7ff3c96c44abd794db9fcbf7596
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2018-12-07 14:46:27 +0000

    Step 13 - Station class tests passing

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -4,13 +4,14 @@ describe Station do
 
     let (:name) {double :name}
     let (:zone) {double :zone}
+    let (:station) {Station.new(name, zone)}
 
     it "checks that the station name variable responds to the station class" do
-        expect(subject).to respond_to(:name)
-    end 
+        expect(station.name).to eq(name)
+    end
 
     it "checks that the station zone variable responds to the station class" do
-        expect(subject).to respond_to(:zone)
-    end 
+        expect(station.zone).to eq(zone)
+    end
 
 end
